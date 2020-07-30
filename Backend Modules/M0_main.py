10654e7f55c258e73b60e7dbd81fba864c3063ad
Baz07 <67526825+Baz07@users.noreply.github.com>
2020-07-29 20:35:08 -0400

    Update M0_main.py

--- Backend Modules/M0_main.py
+++ Backend Modules/M0_main.py
@@ -20,6 +20,7 @@ start = time.time()
 
 def code(X):
     
+    ## Keys
     consumer_key='HJ0eUYLpsznZCHcFMj21Va1UX'
     consumer_secret='o8VyPWxlZEFv6vlmZVxnMYe2FnNcfxrYUw9Ow32eUM81VNYKlk'
     access_token='1217185233029795840-2pH6yQr8aMkCUuy8QLrjnM40poKBqU'
@@ -52,71 +53,69 @@ def code(X):
 
     ## M2
     Cleaned_Tweets = URL_Emoticon_Cleaner(Tweets_list)
-    print(Cleaned_Tweets)
 
-    # ## M3
-    # Lists=[]
-    # Lists = Sentiment_Analyzer(Cleaned_Tweets)
+    ## M3
+    Lists=[]
+    Lists = Sentiment_Analyzer(Cleaned_Tweets)
     
-    # PT_list = Lists[0]
-    # NT_list = Lists[1]
-    # PT_df = Lists[2]
-    # NT_df = Lists[3]
-    # Neut_df = Lists[4]
-    # Pie_dict = Lists[5]
-    # PT_values = Lists[6]
-    # NT_values = Lists[7]
-    # #Zero_values = Lists[8]
+    PT_list = Lists[0]
+    NT_list = Lists[1]
+    PT_df = Lists[2]
+    NT_df = Lists[3]
+    Neut_df = Lists[4]
+    Pie_dict = Lists[5]
+    PT_values = Lists[6]
+    NT_values = Lists[7]
+    #Zero_values = Lists[8]
 
-    # ## M4
-    # Pos_tokens = Pos_Tokenizer(PT_list, User_word)
-    # Neg_tokens = Neg_Tokenizer(NT_list, User_word)
+    ## M4
+    Pos_tokens = Pos_Tokenizer(PT_list, User_word)
+    Neg_tokens = Neg_Tokenizer(NT_list, User_word)
     
-    # ##M5 (Positive)
-    # Pos_Tags=[]
-    # Pos_Tags = Pos_Tagger(Pos_tokens)
-    # Pnoun_list = Pos_Tags[0]
-    # Pverb_list = Pos_Tags[1]
-    # Padj_list = Pos_Tags[2]
+    ##M5 (Positive)
+    Pos_Tags=[]
+    Pos_Tags = Pos_Tagger(Pos_tokens)
+    Pnoun_list = Pos_Tags[0]
+    Pverb_list = Pos_Tags[1]
+    Padj_list = Pos_Tags[2]
 
 
-    # ##M5 (Negative)
-    # Neg_Tags=[]
-    # Neg_Tags = Neg_Tagger(Neg_tokens)
-    # Nnoun_list = Neg_Tags[0]
-    # Nverb_list = Neg_Tags[1]
-    # Nadj_list = Neg_Tags[2]
+    ##M5 (Negative)
+    Neg_Tags=[]
+    Neg_Tags = Neg_Tagger(Neg_tokens)
+    Nnoun_list = Neg_Tags[0]
+    Nverb_list = Neg_Tags[1]
+    Nadj_list = Neg_Tags[2]
 
-    # ## M6
-    # obj9=[]
-    # obj9 = Pos_lemm(Pnoun_list, Pverb_list, Padj_list)
-    # PC_noun_words = obj9[0]
-    # PC_verb_words = obj9[1]
-    # PC_adj_words = obj9[2]
+    ## M6
+    obj9=[]
+    obj9 = Pos_lemm(Pnoun_list, Pverb_list, Padj_list)
+    PC_noun_words = obj9[0]
+    PC_verb_words = obj9[1]
+    PC_adj_words = obj9[2]
 
 
-    # obj10=[]
-    # obj10 = Neg_lemm(Nnoun_list, Nverb_list, Nadj_list)
-    # NC_noun_words = obj10[0]
-    # NC_verb_words = obj10[1]
-    # NC_adj_words = obj10[2]
+    obj10=[]
+    obj10 = Neg_lemm(Nnoun_list, Nverb_list, Nadj_list)
+    NC_noun_words = obj10[0]
+    NC_verb_words = obj10[1]
+    NC_adj_words = obj10[2]
 
-    # WC_dict = Wordlist(PC_noun_words, NC_noun_words, PC_verb_words, NC_verb_words, PC_adj_words, NC_adj_words)
+    WC_dict = Wordlist(PC_noun_words, NC_noun_words, PC_verb_words, NC_verb_words, PC_adj_words, NC_adj_words)
 
-    # ## M8
-    # Recent_News = News()
+    ## M8
+    Recent_News = News()
     
-    # ## For Flask API
-    # temp={}
-    # temp["Pie_Chart"] = Pie_dict
-    # temp["Word_Cloud"] = WC_dict
-    # temp["News"] = Recent_News
+    ## For Flask API
+    temp={}
+    temp["Pie_Chart"] = Pie_dict
+    temp["Word_Cloud"] = WC_dict
+    temp["News"] = Recent_News
     
 
-    # return temp, Tweets_list, Cleaned_Tweets, PT_df, NT_df, Neut_df, PT_values, NT_values, Pos_tokens, Neg_tokens, Pnoun_list, Pverb_list, Padj_list, Nnoun_list, Nverb_list, Nadj_list
+    return temp, Tweets_list, Cleaned_Tweets, PT_df, NT_df, Neut_df, PT_values, NT_values, Pos_tokens, Neg_tokens, Pnoun_list, Pverb_list, Padj_list, Nnoun_list, Nverb_list, Nadj_list
 
 
-code("Air Canada stocks")
 end = (start - time.time())
 print('Time Taken By M0: ', end)
 
